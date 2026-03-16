43c1f1d6c2b1b4dd5bfdf8fd1720cf43a156a722
wlsgml1954 <wlsgml1954@gmail.com>
2026-03-16 19:51:41 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
+import streamlit as st
 import random as rd
 
-print("로또 번호 생성기")
+st.title("로또 번호 생성기")
+st.write("버튼을 누르면 로또 번호가 생성돼요.")
 
-lotto_num = rd.sample(range(1,46),6)
+if st.button("로또 번호 뽑기"):
+  lotto_num = rd.sample(range(1,46),6)
+  lotto_num.sort()
 
-lotto_num.sort()
+  st.subheader(f"이번주 행운의 번호: {lotto_num}")
 
-print(f"이번주 행운의 번호: {lotto_num}")
